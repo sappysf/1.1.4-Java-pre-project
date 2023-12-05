9942d53878efc57033921808251d744d7bfb27f4
myProject <sappysf123@gmail.com>
2023-12-05 22:38:56 +0300

    comit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,84 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private int count;
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        String sql = "create table if not exists users(id integer not null primary key, name varchar(255),lastName varchar(255),age integer)";
+        try (Statement preparedStatement = Util.getConnection().createStatement()) {
+            preparedStatement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        String sql = "drop table if exists users";
+        try(Statement statement = Util.getConnection().createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "insert into users(id,name,lastname,age) values(?,?,?,?)";
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sql)) {
+            preparedStatement.setInt(1, ++count);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setInt(4,age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "delete from users where id = ?";
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sql)) {
+            preparedStatement.setInt(1, (int) id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        String sql = "select * from users";
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sql); ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()){
+                User user = new User(resultSet.getString("name"),resultSet.getString("lastName"),resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                list.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "select * from users";
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sql); ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()){
+                int id = (int) resultSet.getLong("id");
+                removeUserById(id);
+                System.out.println("User c id " + id + " and name " + resultSet.getString("name") + " " + resultSet.getString("lastName") + " was delete");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection getConnection(){
+        Connection connection;
+        final String URL;
+        final String USERNAME;
+        final String PASSWORD;
+        FileInputStream fileInputStream;
+        Properties properties = new Properties();
+        try{
+            fileInputStream = new FileInputStream("src/main/resources/config.properties");
+            properties.load(fileInputStream);
+            URL = properties.getProperty("URL");
+            USERNAME = properties.getProperty("USERNAME");
+            PASSWORD = properties.getProperty("PASSWORD");
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if(connection != null){
+                System.out.println("Correct");
+            }
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
