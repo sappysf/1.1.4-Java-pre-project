ae7806a3edcfb75631bd0ae638cc002760ca6492
myProject <sappysf123@gmail.com>
2023-12-06 13:48:20 +0300

    update

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -12,22 +12,15 @@ public class Util {
     // реализуйте настройку соеденения с БД
     public static Connection getConnection(){
         Connection connection;
-        final String URL;
-        final String USERNAME;
-        final String PASSWORD;
-        FileInputStream fileInputStream;
-        Properties properties = new Properties();
+        final String URL = "jdbc:mysql://localhost:3306/newtable";
+        final String USERNAME = "root";
+        final String PASSWORD = "root";
         try{
-            fileInputStream = new FileInputStream("src/main/resources/config.properties");
-            properties.load(fileInputStream);
-            URL = properties.getProperty("URL");
-            USERNAME = properties.getProperty("USERNAME");
-            PASSWORD = properties.getProperty("PASSWORD");
             connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
             if(connection != null){
                 System.out.println("Correct");
             }
-        } catch (IOException | SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return connection;
