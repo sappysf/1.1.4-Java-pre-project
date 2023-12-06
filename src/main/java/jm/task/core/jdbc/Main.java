de2bd4731126f79ad7ee79100b3f5f38fb96792a
myProject <sappysf123@gmail.com>
2023-12-06 14:04:40 +0300

    update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -16,7 +16,7 @@ public class Main {
         userDao.createUsersTable();
 
         userDao.saveUser("Name1", "LastName1", (byte) 20);
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name2", "LastName2", (byte) 31);
         userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
 
